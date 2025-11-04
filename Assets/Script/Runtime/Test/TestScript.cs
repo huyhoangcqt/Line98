cb76949c243ce8b502518c9855305faf39c83c05
HUYHOANG\huyho <huyhoangcqt@gmail.com>
2025-11-04 07:53:44 +0700

    Add test script

--- Assets/Script/Runtime/Test/TestScript.cs
+++ Assets/Script/Runtime/Test/TestScript.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using YellowCat.Utils;
 
-public class TestScript : MonoBehaviour
+
+public class TestScript : MonoSingletonTemplate<TestScript>
 {
     private void Start() {
         print(Camera.main.WorldToScreenPoint(transform.position));
+        Debugger.Warning(LogTags.Battle, "This is a warning message from TestScript.");
     }
 }
